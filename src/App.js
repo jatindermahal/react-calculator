fc41f3dc170188593be12e09cc9ffeda632f8760
Jatinderpal Singh <jatindermahal11@gmail.com>
2022-07-07 18:19:59 -0400

    fixed format

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import Button from "./Button";
 import Operation from "./Operation";
 
 //calculations:
-const evalOperation = ({currentInput,previousInput,operation})=>{
+const evalOperation = ({ currentInput, previousInput, operation }) => {
   const curr = parseFloat(currentInput);
   const prev = parseFloat(previousInput);
-  if(isNaN(curr) || isNaN(prev)){
-    console.log(curr,"   ",prev)
-    return "";}
+  if (isNaN(curr) || isNaN(prev)) {
+    console.log(curr, "   ", prev);
+    return "";
+  }
   let result = null;
-  switch(operation){
+  switch (operation) {
     case "+":
       result = prev + curr;
       break;
@@ -29,141 +30,166 @@ const evalOperation = ({currentInput,previousInput,operation})=>{
       break;
   }
   return result.toString();
-}
+};
 
 //updating state based on input type
 //state is state which has all three variables we need
 //action contains data with info which button is pressed
-const calcOutput = (state,action)=>{
+const calcOutput = (state, action) => {
   switch (action.type) {
     case "add-num":
-
       //to make sure we can add digits after avaluating
-      if(state.clear) 
-      return{
-        ...state,
-        currentInput:action.data.digit,
-        clear:false}
-
-      if((action.data.digit === "0" || action.data.digit === "00")  && (state.currentInput === "0" || state.currentInput === "00"))
-        return state;
-
-      if(state.currentInput == null && action.data.digit ==="00")
-        return state;
-
-      if(action.data.digit === "." && (state.currentInput == null || state.currentInput.includes(".")))
-        return state;
-        
-      return {
-        ...state,
-        currentInput: `${state.currentInput || ""}${action.data.digit}`
-      }
-
-    case "all-clear":      
-        return {};
-
-        case "clear-line":
-          if(state.clear)
-            return{
-              ...state,
-              clear:false,
-              currentInput:null
-            }
-          
-          if(state.currentInput == null)
-            return state;
-    
-          return{
-            ...state,
-            currentInput:null
-          }
-
-    case "backspace":
-      if(state.clear)
-        return{
-          ...state,
-          clear:false,
-          currentInput:null
-        }
-      
-      if(state.currentInput == null)
-        return state;
-
-      if(state.currentInput.length === 1)
+      if (state.clear)
         return {
           ...state,
-          currentInput:null
-        }
-      return{
+          currentInput: action.data.digit,
+          clear: false,
+        };
+
+      if (
+        (action.data.digit === "0" || action.data.digit === "00") &&
+        (state.currentInput === "0" || state.currentInput === "00")
+      )
+        return state;
+
+      if (state.currentInput == null && action.data.digit === "00")
+        return state;
+
+      if (
+        action.data.digit === "." &&
+        (state.currentInput == null || state.currentInput.includes("."))
+      )
+        return state;
+
+      return {
         ...state,
-        currentInput:state.currentInput.slice(0,-1)
-      }
+        currentInput: `${state.currentInput || ""}${action.data.digit}`,
+      };
+
+    case "all-clear":
+      return {};
+
+    case "clear-line":
+      if (state.clear)
+        return {
+          ...state,
+          clear: false,
+          currentInput: null,
+        };
+
+      if (state.currentInput == null) return state;
+
+      return {
+        ...state,
+        currentInput: null,
+      };
+
+    case "backspace":
+      if (state.clear)
+        return {
+          ...state,
+          clear: false,
+          currentInput: null,
+        };
+
+      if (state.currentInput == null) return state;
+
+      if (state.currentInput.length === 1)
+        return {
+          ...state,
+          currentInput: null,
+        };
+      return {
+        ...state,
+        currentInput: state.currentInput.slice(0, -1),
+      };
 
     case "eval":
-
-      if(state.previousInput == null || state.currentInput == null || state.operation == null)
+      if (
+        state.previousInput == null ||
+        state.currentInput == null ||
+        state.operation == null
+      )
         return state;
 
-      return{
+      return {
         ...state,
         previousInput: null,
-        operation:null,
+        operation: null,
         currentInput: evalOperation(state),
-        clear:true
-      }
+        clear: true,
+      };
 
     case "operation":
-      if(state.currentInput == null && state.previousInput == null)
+      if (state.currentInput == null && state.previousInput == null)
         return state;
 
-      if(state.currentInput == null){
-        return{
+      if (state.currentInput == null) {
+        return {
           ...state,
-          operation: action.data.operator
-        }
+          operation: action.data.operator,
+        };
       }
 
-      if(state.previousInput == null)
+      if (state.previousInput == null)
         return {
           ...state,
           operation: action.data.operator,
           previousInput: state.currentInput,
-          currentInput:null
-      }
+          currentInput: null,
+        };
       return {
         ...state,
         previousInput: evalOperation(state),
         currentInput: null,
-        operation: action.data.operator
-      }
+        operation: action.data.operator,
+      };
 
     default:
       return {};
-
-
-  
-}
-}
+  }
+};
 
 function App() {
-
-  const [{ currentInput, previousInput, operation}, dispatch] = useReducer(calcOutput,{})
-  // 3 states need to be figured. dispatch is what allows us to change these states. 
+  const [{ currentInput, previousInput, operation }, dispatch] = useReducer(
+    calcOutput,
+    {}
+  );
+  // 3 states need to be figured. dispatch is what allows us to change these states.
   // dispatch calls calOutput and updates values for our state variables initially empty
-  
-  
+
   return (
     <>
       <h1 className="title">React Calculator</h1>
       <div className="calculator">
         <div className="result">
-          <div className="history">{previousInput} {operation}</div>
+          <div className="history">
+            {previousInput} {operation}
+          </div>
           <div className="number">{currentInput}</div>
         </div>
         {/* send type to dispatch to tell on what we need to do if button pressed */}
-        <button onClick={()=>{dispatch({type:"all-clear"})}}>AC</button>
-        <button onClick={()=>{dispatch({type:"clear-line"})}}>CE</button>
-        <button onClick={()=>{dispatch({type:"backspace"})}}>⌫</button>
+        <button
+          onClick={() => {
+            dispatch({ type: "all-clear" });
+          }}
+        >
+          AC
+        </button>
+        <button
+          onClick={() => {
+            dispatch({ type: "clear-line" });
+          }}
+        >
+          CE
+        </button>
+        <button
+          onClick={() => {
+            dispatch({ type: "backspace" });
+          }}
+        >
+          ⌫
+        </button>
         <Operation dispatch={dispatch} operator="/" />
         <Button dispatch={dispatch} digit="7" />
         <Button dispatch={dispatch} digit="8" />
@@ -180,10 +206,15 @@ function App() {
         <Button dispatch={dispatch} digit="00" />
         <Button dispatch={dispatch} digit="0" />
         <Button dispatch={dispatch} digit="." />
-        <button onClick={()=>{dispatch({type:"eval"})}}>=</button>
+        <button
+          onClick={() => {
+            dispatch({ type: "eval" });
+          }}
+        >
+          =
+        </button>
       </div>
     </>
-
   );
 }
 
